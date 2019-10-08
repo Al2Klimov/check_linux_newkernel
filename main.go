6c9ae4d5a922fc5a127220b1cb391868b6e9d7f7
Alexander A. Klimov <grandmaster@al2klimov.de>
2019-10-08 19:38:01 +0200

    Monitor also the kernels' ctimes
    fixes #5

--- main.go
+++ main.go
@@ -11,8 +11,10 @@ import (
 	"io/ioutil"
 	"math"
 	"os"
+	"path"
 	"regexp"
 	"strings"
+	"syscall"
 	"time"
 )
 
@@ -21,8 +23,20 @@ type bootTime struct {
 	errs     map[string]error
 }
 
+type kernel struct {
+	cTime, mTime time.Time
+}
+
+func (k *kernel) getLatestTime() time.Time {
+	if k.cTime.After(k.mTime) {
+		return k.cTime
+	} else {
+		return k.mTime
+	}
+}
+
 type kernels struct {
-	kernels map[string]time.Time
+	kernels map[string]kernel
 	errs    map[string]error
 }
 
@@ -82,34 +96,41 @@ func checkLinuxNewkernel() (output string, perfdata PerfdataCollection, errs map
 	if len(krnels.kernels) < 1 {
 		output = "No kernels found (ls /boot/{vmlinuz*,kernel.img})"
 	} else {
-		var latestKernel string
-		var latestKernelMTime time.Time
+		var latestKernelName string
+		var latestKernel kernel
 
-		for kernel, mTime := range krnels.kernels {
-			latestKernel = kernel
-			latestKernelMTime = mTime
+		for kernelName, krnl := range krnels.kernels {
+			latestKernelName = kernelName
+			latestKernel = krnl
 			break
 		}
 
-		for kernel, mTime := range krnels.kernels {
-			if mTime.After(latestKernelMTime) {
-				latestKernel = kernel
-				latestKernelMTime = mTime
+		for kernelName, krnl := range krnels.kernels {
+			if krnl.getLatestTime().After(latestKernel.getLatestTime()) {
+				latestKernelName = kernelName
+				latestKernel = krnl
 			}
 		}
 
-		diff := latestKernelMTime.Sub(btTime.bootTime)
+		diff := latestKernel.getLatestTime().Sub(btTime.bootTime)
 
 		if diff < 0 {
 			output = "No kernels have been installed since boot"
 		} else {
-			output = fmt.Sprintf("The kernel '/boot/%s' has been installed %s after boot", latestKernel, pp.Duration(diff))
+			output = fmt.Sprintf("The kernel '/boot/%s' has been installed %s after boot", latestKernelName, pp.Duration(diff))
 		}
 
 		perfdata = append(perfdata, Perfdata{
 			Label: "mtime_boot_diff",
 			UOM:   "us",
-			Value: float64(diff) / float64(time.Microsecond),
+			Value: float64(latestKernel.mTime.Sub(btTime.bootTime)) / float64(time.Microsecond),
+			Crit:  OptionalThreshold{IsSet: true, Inverted: true, Start: 0, End: math.Inf(1)},
+		})
+
+		perfdata = append(perfdata, Perfdata{
+			Label: "ctime_boot_diff",
+			UOM:   "us",
+			Value: float64(latestKernel.cTime.Sub(btTime.bootTime)) / float64(time.Microsecond),
 			Crit:  OptionalThreshold{IsSet: true, Inverted: true, Start: 0, End: math.Inf(1)},
 		})
 	}
@@ -134,13 +155,26 @@ func getKernels(ch chan<- kernels) {
 		return
 	}
 
-	krnels := map[string]time.Time{}
+	krnels := map[string]kernel{}
+	var errs map[string]error = nil
 
 	for _, entry := range entries {
 		if name := entry.Name(); kernelInBoot.MatchString(name) {
-			krnels[name] = entry.ModTime()
+			krnl := path.Join("/boot", entry.Name())
+
+			var stats syscall.Stat_t
+			if errStat := syscall.Stat(krnl, &stats); errStat != nil {
+				if errs == nil {
+					errs = map[string]error{}
+				}
+
+				errs["stat "+krnl] = errStat
+				continue
+			}
+
+			krnels[name] = kernel{time.Unix(stats.Ctim.Unix()), time.Unix(stats.Mtim.Unix())}
 		}
 	}
 
-	ch <- kernels{kernels: krnels, errs: nil}
+	ch <- kernels{kernels: krnels, errs: errs}
 }
